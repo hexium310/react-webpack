664575d199e4fb4abbe30e84afb4675704084621
Hexin <hexium310@gmail.com>
2020-10-24 15:52:11 +0900

    Enable hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,56 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 const loaders = {
+  babel: {
+    loader: 'babel-loader',
+    options: {
+      plugins: ['react-refresh/babel'],
+    },
+  },
   typescript: {
     loader: 'ts-loader',
   },
 };
 
-module.exports = {
-  entry: './src/index.tsx',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /.tsx?$/,
-        exclude: /node_modules/,
-        use: loaders.typescript,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.ts', '.tsx'],
+module.exports = (_, argv) => {
+  const isDevelopment = argv.mode === 'development';
+
+  return {
+    entry: './src/index.tsx',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /.tsx?$/,
+          exclude: /node_modules/,
+          use: [
+            isDevelopment && loaders.babel,
+            loaders.typescript,
+          ].filter(Boolean),
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.ts', '.tsx'],
+      plugins: [
+        new TsconfigPathsPlugin(),
+      ],
+    },
     plugins: [
-      new TsconfigPathsPlugin(),
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-    }),
-  ],
+      isDevelopment && new ReactRefreshWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+      }),
+    ].filter(Boolean),
+    devServer: {
+      hot: true,
+    },
+  };
 };
